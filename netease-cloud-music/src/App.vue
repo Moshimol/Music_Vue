6243c32bab9fe2081b8d1d9fe0f57964acf4bc36
lushitong <lushitong@qq.com>
2020-12-04 16:18:52 +0800

    添加资源文件  添加Navbar

--- netease-cloud-music/src/App.vue
+++ netease-cloud-music/src/App.vue
@@ -1,28 +1,59 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <music-navbar class="appnav"></music-navbar>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import musicNavbar from "components/context/musicNavbar/MusicNavbar";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    musicNavbar,
+  },
+};
 </script>
 
 <style>
+/* 整体的css导入 */
+@import "./assets/css/base.css";
+
+::-webkit-scrollbar {
+  display: none;
+}
+
+.appnav {
+  position: relative;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  background-color: #fff;
+  box-shadow: 0 -3px 5px rgba(0, 0, 0, 0.7);
+  z-index: 19;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  overflow-x: hidden;
+  background-color: #f0efef;
+}
+
+.v-enter {
+  opacity: 0;
+  /* 进来的时候从右 */
+  transform: translateX(100%);
+}
+
+.v-leave-to {
+  opacity: 0;
+  /* 离开的时候向左 */
+  transform: translateX(-100%);
+}
+
+/* 动画执行期间 */
+.v-enter-active,
+.v-leave-active {
+  /* 添加动画效果 */
+  transition: all 0.2s linear;
 }
 </style>
---
+++ netease-cloud-music/src/components/context/musicNavbar/MusicNavBar.vue
@@ -0,0 +1,73 @@
+<template>
+  <navbar id="navbar">
+    <div id="leftbox" @click="leftPopup" slot="left">
+      <img src="/assets/img/common/hanbaotu.png" alt="" />
+    </div>
+    <div slot="center">
+      <span
+        class="navbarTitle"
+        v-for="(item, index) in titleList"
+        :key="index"
+        @click="itemClick(index)"
+        :class="{ activeTitle: index === currentIndex }"
+      >
+        {{ item }}
+      </span>
+    </div>
+    <div id="rightbox" slot="right" @click="toSearch">
+      <img src="/assets/img/common/sousuo.png" alt="" />
+    </div>
+  </navbar>
+</template>
+
+<script>
+import navbar from "./components/common/navbar/navbar"; // 导航栏组件
+export default {
+  name: "MusicNavbar",
+  data() {
+    return {
+      titleList: ["我的", "发现", "云村", "视频"],
+      currentIndex: 1,
+    };
+  },
+  methods: {
+    leftPopup() {
+      this.$EventBus.$emit("openPopup", () => {}); // 事件总线
+    },
+
+    // 搜索组件
+    toSearch() {},
+
+    // 组件切换
+    itemClick(index) {},
+  },
+  components: {
+    navbar,
+  },
+};
+</script>
+<style scoped>
+#leftbox img {
+  width: 20px;
+  height: 20px;
+  margin-top: 11px;
+}
+#rightbox img {
+  width: 20px;
+  height: 20px;
+  margin-top: 11px;
+}
+
+#navbar {
+  box-shadow: 0 -2px 5px rgba(0, 0, 0, 0.4) !important;
+}
+.navbarTitle {
+  font-size: 15px;
+  padding: 0 7px;
+  color: rgb(116, 116, 116);
+}
+.activeTitle {
+  font-size: 18px;
+  color: #000;
+}
+</style>
